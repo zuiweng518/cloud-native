3429dc156a25574790a8d27c8936f98229fa06fd
shenghao <mashenghao@yahoo.cn>
2023-06-11 20:50:47 +0800

    Update main.go

--- 模块三作业/main.go
+++ 模块三作业/main.go
@@ -1,1 +1,30 @@
+package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/", Index)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("http listen failed")
+	}
+}
+func Index(w http.ResponseWriter, r *http.Request) {
+
+	ip := strings.Split(r.RemoteAddr, ":")[0]
+	version := os.Getenv("VERSION")
+	r_header := r.Header
+	w.Header().Set("IP", ip)
+	w.Header().Set("Version", version)
+	for k, v := range r_header {
+		w.Header().Set(k, v[0])
+
+	}
+	w.WriteHeader(200)
+
+}
